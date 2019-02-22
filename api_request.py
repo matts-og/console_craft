0e5f231310150538657bc647e257662c30da45b8
Matthew Smith <matthew.smith@opengear.com>
2019-02-22 17:13:34 +1000

    As demo'd at OG_hack 2019
    After using '/jsp get-consoles' to generate the console server
    staircase, break the block under the sign with the hostname of the
    console server to load the ports. Then break the block under the sign
    that labels the port to toggle the configuration between
    consoleServer or disabled.

--- api_request.py
+++ api_request.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 # coding: utf-8
 import sys
+import json
 import opengear.imSession as ims
 s = ims.ImSession(sys.argv[1], debug=False)
-r = s.get(sys.argv[2])
-print(r.text)
+r = None
+if sys.argv[2] == 'get':
+    r = s.get(sys.argv[3])
+if sys.argv[2] == 'put':
+    data = json.load(sys.stdin)
+    #print(json.dumps(data))
+    r = s.put(sys.argv[3], json.dumps(data))
+if r != None:
+    print(r.text)
--- console-craft.js
+++ console-craft.js
@@ -1,6 +1,11 @@
 var http = require('http');
+var utils = require('utils');
 
-var consoles = {};
+var consoles_registry = {};
+var ports_registry = {};
+events.on(org.bukkit.event.block.BlockBreakEvent, handleBlockBreak);
+var autotest_dir = new java.io.File("/home/matts/data/src/auto-test");
+
 
 function invokeLater(fn) {
     if (__plugin.bukkit) {
@@ -20,15 +25,24 @@ function get_ckey(d) {
     return ckey;
 }
 
-function api_request_get(player, hostname, endpoint) {
-    var autotest_dir = new java.io.File("/home/matts/data/src/auto-test");
-    var p1 = java.lang.Runtime.getRuntime().exec(
-        ["./api_request.py", hostname, endpoint], [], autotest_dir);
-    var returnVal = p1.waitFor();
-    if (returnVal != 0) {
-        echo(player, hostname + ' api_request failed');
-        return [false, {}];
-    }
+function get_ckey_xyz(x,y,z) {
+    var ckey = 'loc_'+x+'_'+y+'_'+z;
+    return ckey;
+}
+
+function get_ckey_of_block(block) {
+    var x = block.getX();
+    var y = block.getY();
+    var z = block.getZ();
+    return get_ckey_xyz(x, y, z);
+}
+
+function read_api_result(player, hostname, p1) {
+    //var returnVal = p1.waitFor();
+    //if (returnVal != 0) {
+    //    echo(player, hostname + ' api_request failed');
+    //    return [false, {}];
+    //}
     var p1in = new java.io.BufferedReader(new java.io.InputStreamReader(p1.getInputStream()));
     var responseBody = ''
     var line;
@@ -43,15 +57,51 @@ function api_request_get(player, hostname, endpoint) {
         responseJSON = JSON.parse(responseBody);
         json_ok = true;
     } catch (err) {
-        echo(player, err.message);
+        echo(player, hostname + ': ' + err.message);
     }
     return [json_ok, responseJSON];
-
 }
 
-function show_ports(player, hostname, devclass, d) {
+function api_request_get(player, hostname, endpoint) {
+    var p1 = java.lang.Runtime.getRuntime().exec(
+        ["./api_request.py", hostname, 'get', endpoint], [], autotest_dir);
+    return read_api_result(player, hostname, p1);
+}
+
+function api_request_send(player, hostname, method, endpoint, json_obj) {
+    var p1 = java.lang.Runtime.getRuntime().exec(
+        ["./api_request.py", hostname, method, endpoint], [], autotest_dir);
+    var p1out = new java.io.BufferedWriter(new java.io.OutputStreamWriter(p1.getOutputStream()));
+    p1out.write(JSON.stringify(json_obj));
+    p1out.flush();
+    p1out.close();
+    return read_api_result(player, hostname, p1);
+}
+
+function show_port(port, portr, d) {
+    var port_block = blocks.wool.lightgray;
+    var devclass = portr.console.devclass;
+    if (port.mode != 'disabled') {
+        port_block = blocks.wool.cyan;
+    }
+    var baud;
+    if (devclass == 'platypus') {
+        baud = port.baudrate;
+    } else {
+        baud = port.hardwareSettings.uart.baud;
+    }
+    d.box(port_block)
+    .up()
+    .signpost([port.label, port.mode, baud])
+    .down()
+}
+
+function show_ports(player, console, d) {
     var api_result;
     var ports;
+    var hostname = console.hostname;
+    var devclass = console.devclass;
+    d.box(blocks.cobweb);
     if (devclass == 'platypus') {
         api_result = api_request_get(player, hostname, "ports");
         if (!api_result[0]) {
@@ -69,26 +119,27 @@ function show_ports(player, hostname, devclass, d) {
         //echo(player, JSON.stringify(json.serialports[0]));
         ports = json.serialports;
     }
-
+    d.box(blocks.air);
     var ports_len = ports.length;
     for (var i = 0; i < ports_len; i++) {
         var port = ports[i];
-        var port_block = blocks.wool.lightgray;
-        if (port.mode != 'disabled') {
-            port_block = blocks.wool.cyan;
-        }
-        d.down().back()
+        d.down().back(2)
+        .box(blocks.air, 2,5,3)
         .box(blocks.iron, 1,1,3)
-        .up().fwd(2)
-        .box(port_block)
-        .up()
-        .signpost([port.label, port.mode])
-        .down().right().back();
+        .up().fwd(2);
+        var port_ckey = get_ckey(d);
+        ports_registry[port_ckey] = {
+            'console' : console,
+            'id' : port.id,
+            'mode' : port.mode
+        };
+        show_port(port, ports_registry[port_ckey], d);
+        d.right();
     }
 }
 
 function ping_console(player, console, d) {
-    d.box(blocks.wool.lightgray);
+    d.box(blocks.cobweb);
     var hostname = console.hostname;
     var p1 = java.lang.Runtime.getRuntime().exec("ping -c 1 " + hostname);
     var returnVal = p1.waitFor();
@@ -115,6 +166,8 @@ function load_console(player, console, d) {
         if (!ping_console(player, console, d)) {
             return;
         }
+        d.box(blocks.cobweb);
+
         var api_result = api_request_get(player, hostname, "system/version");
         if (!api_result[0]) {
             return;
@@ -124,26 +177,169 @@ function load_console(player, console, d) {
         if ('etc_version' in responseJSON.system_version) {
             devclass = responseJSON.system_version.etc_version.split(" ")[0].split("/")[1];
         }
+        console.devclass = devclass;
 
         d.box(blocks.wool.lime)
         .up().signpost(['Firmware Version:',
             responseJSON.system_version.firmware_version,
             "Class:",
             devclass])
-        .down().back().right();
-        show_ports(player, hostname, devclass, new Drone(d.x, d.y, d.z, d.dir, d.world));
+        .down().right();
+        show_ports(player, console, new Drone(d.x, d.y, d.z, d.dir, d.world));
     });
+}
+
+function ports_url_root(port) {
+    var res = 'serialPorts/';
+    if (port.console.devclass == 'platypus') {
+        res = 'ports/';
+    }
+    return res;
+}
+
+function port_root(port) {
+    var res = 'serialport';
+    if (port.console.devclass == 'platypus') {
+        res = 'port';
+    }
+    return res;
+}
+
+function ports_root(port) {
+    var res = 'serialports';
+    if (port.console.devclass == 'platypus') {
+        res = 'ports';
+    }
+    return res;
+}
+
+function get_port(player, port) {
+    // set port as consoleServer
+    return api_request_get(player, port.console.hostname, ports_url_root(port) + port.id);
+}
+
+function enable_port(player, port, d) {
+    invokeLater(function(){
+        d.box(blocks.cobweb);
+        var request_res = get_port(player, port);
+        if (!request_res[0]) {
+            return;
+        }
+        var port_obj = request_res[1];
+        d.box(blocks.grass_tall);
+        //echo(player, JSON.stringify(port_obj));
+        port_obj[port_root(port)].mode = 'consoleServer';
+        port_obj[port_root(port)].hardwareSettings.uart.baud = '115200';
+        port_obj[port_root(port)].modeSettings = {
+            "consoleServer": {
+                "ssh": {
+                "enabled": true,
+                "unauthenticated": false
+                },
+                "telnet": {
+                "enabled": false,
+                "unauthenticated": false
+                },
+                "general": {
+                "replaceBackspace": false,
+                "escapeChar": "~",
+                "singleConnection": false,
+                "accumulateMS": 0,
+                "powerMenuEnabled": false
+                },
+                "portShare": {
+                "password": "",
+                "authentication": false,
+                "enabled": false,
+                "encryption": false
+                },
+                "rfc2217": {
+                "enabled": false
+                },
+                "webShell": {
+                "enabled": false
+                },
+                "tcp": {
+                "enabled": false
+                },
+                "ipAlias": {
+                "wan": [],
+                "oobfo": [],
+                "wlan": [],
+                "lan": []
+                }
+            }
+        };
+        var send_res = api_request_send(player, port.console.hostname, 'put',
+            ports_url_root(port) + port.id, port_obj);
+        if (send_res[0]) {
+            //echo(player, 'Got ' + JSON.stringify(send_res[1]));
+            show_port(send_res[1][port_root(port)], port, d);
+            port.mode = send_res[1][port_root(port)].mode
+        }
+    });
+}
+
+function disable_port(player, port, d) {
+    invokeLater(function(){
+        d.box(blocks.cobweb);
+        var request_res = get_port(player, port);
+        if (!request_res[0]) {
+            return;
+        }
+        var port_obj = request_res[1];
+        d.box(blocks.grass_tall);
+        //echo(player, JSON.stringify(port_obj));
+        port_obj[port_root(port)].mode = 'disabled';
+        port_obj[port_root(port)].hardwareSettings.uart.baud = '9600';
+        port_obj[port_root(port)].modeSettings = {
+            "disabled": {}
+        };
+        var send_res = api_request_send(player, port.console.hostname, 'put',
+            ports_url_root(port) + port.id, port_obj);
+        if (send_res[0]) {
+            //echo(player, 'Got ' + JSON.stringify(send_res[1]));
+            show_port(send_res[1][port_root(port)], port, d);
+            port.mode = send_res[1][port_root(port)].mode
+        }
+    });
+}
+
+function handleBlockBreak(event, cancel) {
+    var block = event.block;
+    var ckey = get_ckey_of_block(block);
+    if (ckey in consoles_registry) {
+        cancel();
+        var console = consoles_registry[ckey];
+        echo(event.player, "Loading console " + console.hostname);
+        load_console(event.player, console,
+            new Drone(block.getX(), block.getY(), block.getZ(), console.dir, console.world));
+    } else if (ckey in ports_registry) {
+        cancel();
+        var port = ports_registry[ckey];
+        echo(event.player, "Toggling port " + port.id);
+        if (port.mode == 'disabled') {
+            enable_port(event.player, port,
+                new Drone(block.getX(), block.getY(), block.getZ(), port.console.dir, port.console.world));
+        } else {
+            disable_port(event.player, port,
+                new Drone(block.getX(), block.getY(), block.getZ(), port.console.dir, port.console.world));
+        }
+    }
 }
 
 command('get-consoles', function(parameters, player) {
     var do_load = false;
     if (parameters.length > 0) {
-        if (parameters[0] == 'load') {
+        player = utils.player(parameters[0]);
+    }
+    if (parameters.length > 1) {
+        if (parameters[1] == 'load') {
             do_load = true;
         }
     }
-    consoles = {};
-    var d = new Drone(player);
+    consoles_registry = {};
+    var d = new Drone(player).fwd(2).box(blocks.cobweb);
     echo(player, 'TEST');
     http.request('http://matts.bne.opengear.com/cgi-bin/axfr',function(responseCode, responseBody){
         echo(player, 'HTTP response code: ' + responseCode.toString())
@@ -152,6 +348,7 @@ command('get-consoles', function(parameters, player) {
             echo(player, key);
             d.box(blocks.stairs.quartz)
             .fwd()
+            .box(blocks.air, 4,5,3)
             .box(blocks.iron, 4,1,3)
             .up().fwd(2).right()
             .box(blocks.iron)
@@ -159,13 +356,14 @@ command('get-consoles', function(parameters, player) {
             .signpost([key])
             .down();
             var ckey = get_ckey(d);
-            consoles[ckey] = {
+            consoles_registry[ckey] = {
                 'hostname' : key,
                 'dir' : d.dir,
+                'world' : d.world
             };
-            var console = consoles[ckey];
+            var console = consoles_registry[ckey];
             if (do_load) {
-                load_console(player, consoles[ckey], new Drone(d.x, d.y, d.z, d.dir, d.world));
+                load_console(player, consoles_registry[ckey], new Drone(d.x, d.y, d.z, d.dir, d.world));
             } else {
                 d.right();
                 ping_console_later(player, console, new Drone(d.x, d.y, d.z, d.dir, d.world));
@@ -179,10 +377,10 @@ command('get-consoles', function(parameters, player) {
 command('load-console', function(parameters, player) {
     var d = new Drone(player);
     var ckey = get_ckey(d);
-    if (!ckey in consoles) {
+    if (!(ckey in consoles_registry)) {
         echo('Must point at block below signpost with console name');
         return;
     }
-    var console = consoles[ckey];
-    load_console(player, console, new Drone(d.x, d.y, d.z, console.dir, d.world));
+    var console = consoles_registry[ckey];
+    load_console(player, console, new Drone(d.x, d.y, d.z, console.dir, console.world));
 });
